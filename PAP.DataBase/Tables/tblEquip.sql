6518733c9499a0329e0e790c12c268ae087227eb
tomas marques <marquestomas1@hotmail.com>
2020-02-13 18:27:33 +0000

    Tabela Salas - DB

--- PAP.DataBase/Tables/tblEquip.sql
+++ PAP.DataBase/Tables/tblEquip.sql
@@ -2,8 +2,9 @@
 (
 	[id_equip] INT NOT NULL PRIMARY KEY identity,
 	[desc] varchar(50) NOT NULL,
-	[Quant] int not null,
 	[disp] BIT default 0 not null,
 	[id_cat] int not null,
-	constraint [FK_tblEquip_tblCat] foreign key ([id_cat]) references [tblCat]([id_cat])
+	[id_sala] INT NULL, 
+    constraint [FK_tblEquip_tblCat] foreign key ([id_cat]) references [tblCat]([id_cat]),
+	constraint [FK_tblEquip_tblSalas] foreign key ([id_sala]) references [tblSalas]([id_sala])
 )
---
+++ PAP.DataBase/Tables/tblSalas.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblSalas]
+(
+	[id_sala] INT NOT NULL PRIMARY KEY identity, 
+    [nome] NCHAR(10) NOT NULL,
+)
